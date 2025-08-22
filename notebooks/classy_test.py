5b7d252ca318380519361de72e923fd1b31604ef
MarMos <markus.mosbech@gmail.com>
2025-08-22 10:47:16 +0200

    Fixed typos

--- notebooks/classy_test.py
+++ notebooks/classy_test.py
@@ -5,7 +5,7 @@ try:
     cosmo = Class()
     cosmo.set({'output':'mPk,tCl'})
     Cl = cosmo.raw_cl()
-    print(f'Classy succesfully rum, CMB Cls computed up to l={Cl['ell'][-1]}')
+    print(f'Classy succesfully run, CMB Cls computed up to l={Cl['ell'][-1]}')
 except Exception as error:
     print("Something has gone wrong!")
     print("Error message:", error)
